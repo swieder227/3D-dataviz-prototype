27aa53bd49315f66e9e200a57399eb358f731e1a
Seth Wieder <swieder227@gmail.com>
2016-10-27 16:57:49 -0400

    add hotspots with raycast click detection. add docs.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-var container, scene, renderer, camera, controls, stats, grid_object, all_graphs = [], all_labels = [];
+/* THREE setup */
+var container, scene, renderer,
+/* camera related + helpers */
+    camera, controls, stats,
+/* objects in scene */
+    grid_object, all_graphs = [], all_labels = [], all_hotspots = [],
+/* mouse tracking */
+    mouse, raycaster;
 
 /**
  * The relative size of the grid/graph in 3D world dimensions
@@ -17,10 +24,10 @@ const GRID_DIMENSIONS = {
 const GRAPH_COLORS = [0x5E648C, 0x98B9DA, 0x7EA992, 0xA4CBCF];
 
 /**
- * All data to graph a PIMCO story
+ * All data to graph a story
  * @type {Array} An array of data sets
  * @type {Array} data[x][0] - array of data values with [date,value]
- * @type {Array} data[x][1] - two value array with min/max values for createGraphPlane()
+ * @type {Array} data[x][1] - two value array with min/max values for Y axis in createGraphPlane()
  */
 const DATA_BREXIT = [ [data_brexit_gbp, [0,15]], [data_brexit_vix, [0,150]], [data_brexit_uk, [0,15]], [data_brexit_msci, [0,150]] ];
 const DATA_HOUSING = [ [data_housing_usfed, [-1,60]], [data_housing_abx, [0,600]], [data_housing_spcs, [0,600]],[data_housing_sp500, [0,600]] ];
@@ -29,7 +36,7 @@ const DATA_CHINA = [ [data_china_gdp, [0, 60]], [data_china_sp500, [0, 6000]], [
 const DATA_OIL = [ [data_oil_opec, [0, 150000]], [data_oil_non_opec, [0, 150000]], [data_oil_msci, [0,1500]], [data_oil_crude, [0, 150]] ];
 
 /**
- * X/Y Axis labels per PIMCO story
+ * X/Y Axis labels per story
  * @type {Array} An array with two or three entries
  * @type {Array} label[0] - An array of labels for the X axis
  * @type {Array} label[1] - An array of labels for the Y axis
@@ -40,12 +47,20 @@ const LABELS_MONETARY = [ [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014,
 const LABELS_CHINA = [ [2000, 2003, 2006, 2009, 2012, 2015, 2018, 2021], [0, 1, 2, 3, 4, 5, 6] ];
 const LABELS_OIL = [ [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016], [0, 3, 6, 9, 12, 15] ];
 
+
+/**
+ * Constants used for sizing/positiong the camera
+ * @type {Number}
+ */
+const VIEW_SIZE = Math.max(GRID_DIMENSIONS.width * 1.5, GRID_DIMENSIONS.height * 1.5);
+const ASPECT = window.innerWidth / window.innerHeight;
+
 /**
  * Default camera positions
  * @type {THREE.Vector3} { x, y, z }
  */
-const POSITION_2D = new THREE.Vector3(0, 0, -1);
-const POSITION_3D = new THREE.Vector3(0.8, 0.55, -1);
+const POSITION_2D = new THREE.Vector3(0, 0, -VIEW_SIZE);
+const POSITION_3D = new THREE.Vector3(0.8, 0.55, -VIEW_SIZE);
 
 /**
  * Init all ThreeJS components
@@ -67,35 +82,53 @@ function initialize(){
   container.appendChild( renderer.domElement );
 
   // Camera
-  // size is set as the graph dimensions w/ padding
-  let view_size = Math.max(GRID_DIMENSIONS.width * 1.5, GRID_DIMENSIONS.height * 1.5);
-  let aspect = window.innerWidth / window.innerHeight;
-  camera = new THREE.OrthographicCamera( -aspect*view_size / 2, aspect*view_size / 2, view_size / 2, -view_size / 2, -1000, 1000 );
+  camera = new THREE.OrthographicCamera( -ASPECT*VIEW_SIZE / 2, ASPECT*VIEW_SIZE / 2, VIEW_SIZE / 2, -VIEW_SIZE / 2, 0, 500 );
 
   //zoom
   camera.zoom = 0.85;
   camera.updateProjectionMatrix();
   // position
-  camera.position.x = POSITION_3D.x;
-  camera.position.y = POSITION_3D.y;
-  camera.position.z = POSITION_3D.z;
+  camera.position.x = POSITION_2D.x;
+  camera.position.y = POSITION_2D.y;
+  camera.position.z = POSITION_2D.z;
 
   // Controls
   controls = new THREE.OrbitControls( camera, renderer.domElement );
-  // controls.enabled = false;
-
-  /*// Helper
-  var axis_helper = new THREE.AxisHelper( 5 );
-  axis_helper.position.z = 40;
-  scene.add(axis_helper);*/
 
   // Stats
   stats = new Stats();
   container.appendChild(stats.dom);
 
+  // Mouse
+  mouse = new THREE.Vector2();
+
+  // Raycaster
+  raycaster = new THREE.Raycaster();
+
   // Graphs, Data, Labels
   setupGraphsForStory(DATA_BREXIT, LABELS_BREXIT);
 
+  // Event Handlers
+  bindEventHandlers();
+
+}
+
+/**
+ * Add JS event handlers
+ */
+function bindEventHandlers(){
+  window.addEventListener( 'mousemove', onMouseMove, false );
+  window.addEventListener( 'mouseup', checkRaycastHotspots, false);
+}
+
+/**
+ * On mouse movement events, update the mouse vector coordinates
+ * Normalizes screen coords to 3d coords.
+ * @param  {Event} event - event object from 'mousemove'
+ */
+function onMouseMove( event ) {
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 }
 
 /**
@@ -300,20 +333,29 @@ function addCurrentGraphsToScene(){
 }
 
 /**
+ * Given an array of THREE objects, remove them from scene and reset array
+ * @param  {Array} scene_objects - the array to reset
+ * @return {Array} empty array to nullify
+ */
+function removeArrayObjectsFromScene(scene_objects = []){
+  for (var i = scene_objects.length - 1; i >= 0; i--) {
+    scene.remove(scene_objects[i]);
+  }
+  return [];
+}
+
+/**
  * Remove all 3D graphs, labels, grids, etc. from the scene
  */
-function removeCurrentGraphsFromScene(){
+function resetCurrentScene(){
   // clear all graphs
-  for (var i = all_graphs.length - 1; i >= 0; i--) {
-    scene.remove(all_graphs[i]);
-  }
-  all_graphs = [];
+  all_graphs = removeArrayObjectsFromScene(all_graphs);
 
   // clear all labels
-  for (var i = all_labels.length - 1; i >= 0; i--) {
-    scene.remove(all_labels[i]);
-  }
-  all_labels = [];
+  all_labels = removeArrayObjectsFromScene(all_labels);
+
+  // clear hotspots
+  all_hotspots = removeArrayObjectsFromScene(all_hotspots);
 
   // clear grid
   scene.remove(grid_object);
@@ -330,8 +372,9 @@ function removeCurrentGraphsFromScene(){
  * @param {Array} labels[1] - Y Axis
  */
 function setupGraphsForStory(dataset, labels){
+
   // clear everything
-  removeCurrentGraphsFromScene();
+  resetCurrentScene();
 
   // setup grid
   grid_object = setupAllGrids(labels[0].length, labels[1].length);
@@ -347,9 +390,9 @@ function setupGraphsForStory(dataset, labels){
   addCurrentGraphsToScene();
 
   // hotspots
-  placeHotspotOnGraph(all_graphs[0], 768);
-  placeHotspotOnGraph(all_graphs[1], 728);
-  placeHotspotOnGraph(all_graphs[3], 2000);
+  all_hotspots.push(placeHotspotOnGraph(all_graphs[0], 0));
+  all_hotspots.push(placeHotspotOnGraph(all_graphs[1], dataset[1][0].length / 2));
+  all_hotspots.push(placeHotspotOnGraph(all_graphs[3], dataset[3][0].length - 1));
 }
 
 /**
@@ -479,8 +522,8 @@ function setupYAxisLabels(labels){
  * @return {THREE.Mesh} The 3D Object to be added to `scene`
  */
 function createHotspot(){
-  var geometry = new THREE.CircleGeometry( 0.5, 32 );
-  var material = new THREE.MeshBasicMaterial( { color: 0xff0000, side: THREE.DoubleSide } );
+  var geometry = new THREE.SphereGeometry( 0.45, 32, 32 );
+  var material = new THREE.MeshBasicMaterial( { color: 0x1374B8, side: THREE.DoubleSide } );
   var circle = new THREE.Mesh( geometry, material );
   return circle;
 }
@@ -489,25 +532,56 @@ function createHotspot(){
  * Creates and positions a hotspot along a graph
  * @param  {THREE.Mesh} graph_object - A THREE object for one of the graph planes
  * @param  {Number} offset_index - the index, offset from 0, to access graph.vertices[i]
+ * @return The hotspot THREE.mesh after positioning and adding to scene
  */
 function placeHotspotOnGraph(graph_object = all_graphs[3], offset_index = 200){
 
   // derive the length of the data from length of graph
   // 1/2 bc graph is a polygon with equal top/bottom vertices.
-  let data_length = graph_object.geometry.vertices.length / 2;
+  let data_length = Math.floor( graph_object.geometry.vertices.length / 2);
   // find vertex along graph
   // verticies[0] is last point, verticies[(dataset.length - 1)] is first point
-  let data_point = Math.round( (data_length - 1) - offset_index );
+  let data_point = (data_length - 1) - Math.floor(offset_index);
   // get Vector3 position
   let vertex_position = graph_object.geometry.vertices[data_point];
 
   // set pos of hotspot
   let hotspot = createHotspot();
   hotspot.position.x = vertex_position.x;
-  hotspot.position.y = vertex_position.y + graph_object.position.y;
+  hotspot.position.y = vertex_position.y + graph_object.position.y + 0.5;
   hotspot.position.z = graph_object.position.z;
 
   scene.add(hotspot);
+
+  return hotspot;
+}
+
+function checkRaycastHotspots(){
+  // calculate objects intersecting the picking ray
+  var intersects = raycaster.intersectObjects( all_hotspots );
+
+  if(intersects.length > 0){
+    console.log(intersects[0]);
+    alert("Hotspot clicked.");
+    // intersects[0].object.material.color.set( 0x0000ff );
+  }
+}
+
+/**
+ * Updates raycast origin/direction based on mouse/camera coords
+ * Generic fn for the animate() loop
+ * @return {[type]} [description]
+ */
+function raycastUpdate(){
+
+  // early exit on first init
+  if(mouse.x === 0 && mouse.y === 0){
+    return false;
+  }
+
+  // update the picking ray with the camera and mouse position
+  raycaster.setFromCamera( mouse, camera );
+
 }
 
 // animation loop
@@ -516,6 +590,7 @@ function animate(){
   controls.update();
   stats.update();
   TWEEN.update();
+  raycastUpdate();
   render();
 }
 
